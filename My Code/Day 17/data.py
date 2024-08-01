2c89e5ca82521d0255a8fbc8a75b30d5d3f20be3
Hanna <mewsis02@gmail.com>
2024-07-31 22:30:02 -0400

    Completed the Day 17 Project!

--- My Code/Day 17/data.py
+++ My Code/Day 17/data.py
@@ -1,14 +1,84 @@
 question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {
+        "question": "In Riot Games 'League of Legends'; the name of Halloween event is called 'The Reckoning'.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "TF2: The Medic will be credited for an assist if heals a Spy that successfully saps a building.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Shang Tsung is a playable character in Mortal Kombat XL.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "In Monster Hunter Generations, guild style is a type of hunting style.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The ADAM collecters in the Bioshock series are known as Little Sisters.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The Fiat Multipla is a drivable car in 'Forza Horizon 3'.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "In the Resident Evil series, Leon S. Kennedy is a member of STARS.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "In 'Call Of Duty: Zombies', you can upgrade the 'Apothicon Servant' in the 'Shadows Of Evil' map.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "In 'Super Mario World', the rhino mini-boss Reznor, "
+                    "is named after the lead singer of the band 'Nine Inch Nails'.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Minecraft wasn't the original name to Minecraft.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "In 'Overwatch', an allied McCree will say 'Step right up' upon using ultimate ability Deadeye.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "'Metal Gear Solid 3: Snake Eater' takes place on Shadow Moses Island.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "In the game 'Subnautica';, a 'Cave Crawler' will attack you.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Hidden in the files for 'Mario Kart Arcade GP'; is a picture of the Beslan school hostage crisis.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The first Maxis game to feature the fictional language 'Simlish'; was The Sims (2000).",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Metal Gear Solid V: The Phantom Pain runs on the Fox Engine.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "In career mode of 'Need for Speed: Underground 2', "
+                    "the first car the player can drive is the BMW M3 GTR.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "During pre-production of 'Super Mario Bros.', Mario originally was meant to shoot bullets.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "In the video game 'Transistor', 'Red' is the name of the main character.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "TF2: The Heavy's voice actor, Gary Schwartz, voices the Demoman as well ",
+        "correct_answer": "True",
+    }
 ]
